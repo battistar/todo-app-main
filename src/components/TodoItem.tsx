e0a3e64364435e6b770e9cf48616d67a57212af6
Samuele Battistella <battistar92@gmail.com>
2023-09-11 13:30:27 +0200

    Add children property to TodoItem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,12 +2,13 @@ import { ReactComponent as IconCross } from 'assets/images/icon-cross.svg';
 import './TodoItem.css';
 
 type TodoItemProps = {
+  children: string;
   toggle?: () => void;
   remove?: () => void;
   checked?: boolean;
 };
 
-const TodoItem = ({ toggle, remove, checked }: TodoItemProps): JSX.Element => {
+const TodoItem = ({ children, toggle, remove, checked }: TodoItemProps): JSX.Element => {
   const handleCheckboxClick = (): void => {
     if (toggle) {
       toggle();
@@ -23,7 +24,7 @@ const TodoItem = ({ toggle, remove, checked }: TodoItemProps): JSX.Element => {
   return (
     <div className="todo-item">
       <input type="checkbox" className="todo-item__checkbox" onClick={handleCheckboxClick} />
-      <p className={`todo-item__text ${checked && 'checked'}`}>Some text</p>
+      <p className={`todo-item__text ${checked && 'checked'}`}>{children}</p>
       <button className="todo-item__remove-button" onClick={handleRemoveClick}>
         <IconCross />
       </button>

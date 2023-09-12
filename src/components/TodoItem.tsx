b91b0f93e9b5269f090a892e1ae93e38a86bd6aa
Samuele Battistella <battistar92@gmail.com>
2023-09-12 18:23:17 +0200

    Change TodoItem component to styled components

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,56 @@
 import { ReactComponent as IconCross } from 'assets/images/icon-cross.svg';
-import './TodoItem.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  padding: 16px 24px;
+
+  & input[type='checkbox'] {
+    appearance: none;
+    margin: 0;
+  }
+`;
+
+const Checkbox = styled.input.attrs({ type: 'checkbox' })`
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  border: 1px solid hsl(233, 11%, 84%);
+
+  &:hover,
+  &:active {
+    border-color: hsl(280, 87%, 65%);
+    cursor: pointer;
+  }
+
+  &:checked {
+    background: hsl(280, 87%, 65%) url('./src/assets/images/icon-check.svg') no-repeat center;
+    border-color: hsl(280, 87%, 65%);
+  }
+`;
+
+const Text = styled.p`
+  flex: 1;
+  color: hsl(235, 19%, 35%);
+  padding-top: 4px;
+
+  &.completed {
+    text-decoration: line-through;
+    color: hsl(233, 11%, 84%);
+  }
+`;
+
+const Button = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
+`;
+
+const StyledIconCross = styled(IconCross)`
+  transform: scale(0.7);
+`;
 
 type TodoItemProps = {
   children: string;
@@ -22,13 +73,13 @@ const TodoItem = ({ children, onToggle, onRemove, completed }: TodoItemProps): J
   };
 
   return (
-    <div className="todo-item">
-      <input type="checkbox" className="todo-item__checkbox" onClick={handleCheckboxClick} defaultChecked={completed} />
-      <p className={`todo-item__text ${completed && 'completed'}`}>{children}</p>
-      <button className="todo-item__remove-button" onClick={handleRemoveClick}>
-        <IconCross className="todo-item__remove-button__icon" />
-      </button>
-    </div>
+    <Container>
+      <Checkbox onClick={handleCheckboxClick} defaultChecked={completed} />
+      <Text className={`${completed && 'completed'}`}>{children}</Text>
+      <Button onClick={handleRemoveClick}>
+        <StyledIconCross />
+      </Button>
+    </Container>
   );
 };
 

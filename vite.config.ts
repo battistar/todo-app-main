966e2e44a681376b0d7b0a9108bf61cb5272872c
Samuele Battistella <battistar92@gmail.com>
2023-09-15 23:06:11 +0200

    Fix VIte base option

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'todo-app-main',
+  base: '/todo-app-main',
   plugins: [react(), svgr(), tsconfigPaths()],
 });
